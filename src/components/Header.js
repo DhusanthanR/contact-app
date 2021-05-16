7889bf773d77b0b0c1b5d75b631b755c41632be2
DhusanthanR <dhusanthan1999g@gmail.com>
2021-05-16 14:15:42 +0530

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className="ui fixed menu">
+            <div className="ui center container" style={{textAlign:'center'}}>
+                <h2 className="center" style={{width:'100%'}}>My Contact</h2>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
